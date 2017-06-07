63ddaf8c862d331c9296fe4bf63642c0e9a92183
alecristia <alecristia@gmail.com>
2017-06-07 09:52:37 +0200

    new ES calculations on subset

--- powerana/subset_ana.R
+++ powerana/subset_ana.R
@@ -1,16 +1,51 @@
 read.csv("ids_ma.csv")->x
+table(x$coder,x$participant_design)
+#new lines don't have ES, they are all within_two
+summary(x$corr) #all NA
+
+x$pooled_SD=x$corr_imp=x$d_calc=x$d_calc_var=x$es_method=NA
+x$corr_imp=0
+
+#between calc
+x$pooled_SD[x$participant_design=="between"]<- sqrt(((x$n_1[x$participant_design=="between"] - 1) * x$SD_1[x$participant_design=="between"] ^ 2 + (x$n_2[x$participant_design=="between"] - 1) * x$SD_2[x$participant_design=="between"] ^ 2) / (x$n_1[x$participant_design=="between"] + x$n_2[x$participant_design=="between"] - 2)) # Lipsey & Wilson, 3.14
+x$es_method[x$participant_design=="between"]  <- "between"
+
+#within_two calc
+#summary(x[x$participant_design=="within_two",c("x_1","x_2","SD_1","SD_2")]) #checking - 41 NAs corresponds to the original studies
+x$pooled_SD[x$participant_design=="within_two"]<- sqrt((x$SD_1[x$participant_design=="within_two"] ^ 2 + x$SD_2[x$participant_design=="within_two"] ^ 2) / 2) # Lipsey & Wilson, 3.14
+x$es_method[x$participant_design=="within_two"]  <- "group_means_two"
+
+x$d_calc <- (x$x_1 - x$x_2) / x$pooled_SD # Lipsey & Wilson (2001)
+x$d_var_calc <- (2 * (1 - x$corr_imp)/ x$n_1) + (x$d_calc ^ 2 / (2 *x$n_1)) # Lipsey & Wilson (2001)
+
+summary(x$d_var_calc) #50 NAs
+x$d_calc[x$coder=="Alex Cristia"]<-x$d[x$coder=="Alex Cristia"]
+x$d_var_calc[x$coder=="Alex Cristia"]<-x$d_var[x$coder=="Alex Cristia"]
+summary(x$d_var_calc) #no NAs
+
+library(metafor)
+x$se=sqrt(x$d_var_calc)
+x$w=1/sqrt(x$d_var_calc)
+original=rma.uni(yi=d_calc, sei=se, weights=w,data=x,subset=c(coder=="Alex Cristia"))
+summary(original)
+
+withNew=rma.uni(yi=d_calc, sei=se, weights=w,data=x)
+summary(withNew)
+
 table(x$infant_type)
 table(x$stim_language)
 table(x$setting)
 table(x$speaker)
 table(x$speech_type)
+table(x$dependent_measure)
+
 subset(x, speech_type != "Filtered" & speech_type != "Synthesized" &
          speaker!="Child\342\200\231s mother" & speaker != "Unfamiliar male" & 
          setting=="Laboratory" & 
          stim_language=="English" & 
-         infant_type=="typical")->relevant
+         infant_type=="typical" & 
+         dependent_measure=="looking_time")->relevant
+selected=rma.uni(yi=d, sei=se, weights=w,data=relevant)
+summary(selected)
 
-library(metafor)
-x$se=sqrt(x$d_var)
-x$w=1/sqrt(x$d_var)
-rma.uni(yi=d, sei=se, weights=w,data=x)
+
